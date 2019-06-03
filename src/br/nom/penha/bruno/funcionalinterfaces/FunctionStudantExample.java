2aebaae721eb694be3d4427b25c5290a2772763a
Bruno C. Penha <bruno.penha@uniriotec.br>
2019-06-03 15:43:09 -0300

    Inclusao de estudo sobre a Interface Function com a classe Studant.

---
+++ src/br/nom/penha/bruno/funcionalinterfaces/FunctionStudantExample.java
@@ -0,0 +1,31 @@
+package br.nom.penha.bruno.funcionalinterfaces;
+
+import br.nom.penha.bruno.data.Student;
+import br.nom.penha.bruno.data.StudentDataBase;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+public class FunctionStudantExample {
+
+    static Function<List<Student>,Map<String,Double>> studantFunction = (students -> {
+       Map<String, Double> studantGradeMap = new HashMap<>();
+
+       students.forEach((student -> {
+
+           if(PredicateStudantExample.p1.test(student)){
+               studantGradeMap.put(student.getName(), student.getGpa());
+           }
+
+       }));
+
+       return studantGradeMap;
+    });
+
+    public static void main(String[] args) {
+
+        System.out.println(studantFunction.apply(StudentDataBase.getAllStudents()));
+    }
+}
