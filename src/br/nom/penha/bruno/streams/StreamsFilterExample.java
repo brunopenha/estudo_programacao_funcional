d3db85717bf2d99a68445ab696404c0e10b5ac3e
Bruno C. Penha <bruno.penha@uniriotec.br>
2019-06-17 01:27:07 -0300

    Inclusão de um exemplo de Operação com Stream usando Filter

---
+++ src/br/nom/penha/bruno/streams/StreamsFilterExample.java
@@ -0,0 +1,20 @@
+package br.nom.penha.bruno.streams;
+
+import br.nom.penha.bruno.data.Student;
+import br.nom.penha.bruno.data.StudentDataBase;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamsFilterExample {
+
+    public static List<Student> filterStudents(){
+        return StudentDataBase.getAllStudents().stream() // Stream<Studant>
+                .filter((student -> student.getGender().equals("female"))) // Stream<Student> filtered by whose gender is female
+                .collect(Collectors.toList());  // List<String>
+    }
+
+    public static void main(String[] args) {
+        filterStudents().forEach(System.out::println);
+    }
+}
