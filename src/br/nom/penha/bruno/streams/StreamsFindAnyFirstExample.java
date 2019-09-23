fa9333eb76b804a4e2d2f0d247c35301ab959561
Bruno C. Penha <bruno.penha@uniriotec.br>
2019-09-23 21:37:53 +0200

    Exemplo utilizando findFirst e findAny

---
+++ src/br/nom/penha/bruno/streams/StreamsFindAnyFirstExample.java
@@ -0,0 +1,42 @@
+package br.nom.penha.bruno.streams;
+
+import br.nom.penha.bruno.data.Student;
+import br.nom.penha.bruno.data.StudentDataBase;
+
+import java.util.Optional;
+
+public class StreamsFindAnyFirstExample {
+
+    public static Optional<Student> findAnyEstudante(double nota){
+        // adam, jenny, emily
+        return StudentDataBase.getAllStudents().stream()
+                .filter( estudante -> estudante.getGpa() >= nota )
+                .findAny();
+    }
+
+    public static Optional<Student> findFirstEstudante(double nota){
+        // adam, jenny, emily
+        return StudentDataBase.getAllStudents().stream()
+                .filter( estudante -> estudante.getGpa() >= nota )
+                .findFirst();
+    }
+
+    public static void main(String[] args) {
+
+        Optional<Student> estudante = findAnyEstudante(3.8);
+
+        if(estudante.isPresent()){
+            System.out.println("Estudante encontrado " + estudante.get());
+        }else{
+            System.out.println("Nimguem foi encontrado");
+        }
+
+        estudante = findFirstEstudante(3.8);
+
+        if(estudante.isPresent()){
+            System.out.println("Estudante encontrado " + estudante.get());
+        }else{
+            System.out.println("Nimguem foi encontrado");
+        }
+    }
+}
