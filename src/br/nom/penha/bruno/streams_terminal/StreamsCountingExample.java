2de14b7b6a959c14801e6950857409f0d1bed0d6
Bruno C. Penha <bruno.penha@uniriotec.br>
2019-11-19 21:29:08 +0100

    Exemplo utilizando Streams Terminal com couting

---
+++ src/br/nom/penha/bruno/streams_terminal/StreamsCountingExample.java
@@ -0,0 +1,19 @@
+package br.nom.penha.bruno.streams_terminal;
+
+import br.nom.penha.bruno.data.StudentDataBase;
+
+import java.util.stream.Collectors;
+
+public class StreamsCountingExample {
+
+    public static long counting(){
+        return StudentDataBase.getAllStudents()
+                .stream()
+                .filter(student -> student.getGpa() >= 3.9)
+                .collect(Collectors.counting());
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Counting: " + counting());
+    }
+}
