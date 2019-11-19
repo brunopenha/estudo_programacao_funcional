452b09692691fd9c4c415e33405e05e355f8be0a
Bruno C. Penha <bruno.penha@uniriotec.br>
2019-11-19 22:37:38 +0100

    Exemplo utilizando Streams Terminal com groupingBy (tipo 2)

--- src/br/nom/penha/bruno/streams_terminal/StreamsGroupingByExample.java
+++ src/br/nom/penha/bruno/streams_terminal/StreamsGroupingByExample.java
@@ -17,17 +17,53 @@ public class StreamsGroupingByExample {
         System.out.println(listMap);
     }
 
-    public static void customizedgroupStudentsByGender(){
+    public static void customizedgroupStudentsByGpa(){
         Map<String,List<Student>> listMap = StudentDataBase.getAllStudents()
                 .stream()
                 .collect(Collectors.groupingBy(student -> student.getGpa() >= 3.8? "Muito bom!" : "Mediocre"));
 
-        System.out.println(listMap);
+        System.out.println("Lista com alunos female[] e male[]: \n" + listMap);
+    }
+
+    public static void customizedgroupStudentsByGpaAndByGradeLevel(){ //two level grouping
+        Map<Integer,Map<String,List<Student>>> listMap = StudentDataBase.getAllStudents()
+                .stream()
+                .collect(Collectors.groupingBy(Student::getGradeLevel,
+                         Collectors.groupingBy(student -> student.getGpa() >= 3.8? "Muito bom!" : "Mediocre")));
+
+        System.out.println(" Lista com alunos por Grade Level com o Melhor e pior GPA:\n "+ listMap);
+    }
+
+    public static void customizedgroupStudentsByGradeLevelAndSumOfNotebooks(){ //two level grouping
+        Map<Integer,Integer> listMap = StudentDataBase.getAllStudents()
+                .stream()
+                .collect(Collectors.groupingBy(Student::getGradeLevel,
+                         Collectors.summingInt(Student::getNotebooks)));
+
+        System.out.println("Lista com o total de notebooks por Grade Level\n " + listMap);
+    }
+
+    public static void customizedgroupStudentsByNameAndSumOfNotebooks(){ //two level grouping
+        Map<String,Integer> listMap = StudentDataBase.getAllStudents()
+                .stream()
+                .collect(Collectors.groupingBy(Student::getName,
+                        Collectors.summingInt(Student::getNotebooks)));
+
+        System.out.println("Lista com o total de notebooks por Grade Level\n " + listMap);
     }
 
     public static void main(String[] args) {
 
+        System.out.println("*************************");
         groupStudentsByGender();
-        customizedgroupStudentsByGender();
+        System.out.println("*************************");
+        customizedgroupStudentsByGpa();
+        System.out.println("*************************");
+        customizedgroupStudentsByGpaAndByGradeLevel();
+        System.out.println("*************************");
+        customizedgroupStudentsByGradeLevelAndSumOfNotebooks();
+        System.out.println("*************************");
+        customizedgroupStudentsByNameAndSumOfNotebooks();
+        System.out.println("*************************");
     }
 }
